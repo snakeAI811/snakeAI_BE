455e3e657afd6cbce55b4707fa46f5d61e2649be
TopDev9018 <superfullstackdev9999@gmail.com>
2025-09-01 09:51:55 +1100

    Fix: matched module stable and db migrate

--- frontend/server.js
+++ frontend/server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from "dist" or "build" folder
-app.use(express.static(path.join(__dirname, "dist"))); // or "build" if React
+app.use(express.static(path.join(__dirname, "build"))); // or "build" if React
 
-// Fallback for React Router (SPA)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+// Fallback for React Router (SPA) express v5
+app.get("*splat", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
